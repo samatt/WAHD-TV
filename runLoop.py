2f6f08cab444c154089eb40eb0dd11face7217ef
Surya Mattu <surya@suryamattu.com>
2015-01-31 16:08:18 -0500

    Added kill process on exit

--- runLoop.py
+++ runLoop.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import random
 import time
+import sys
 
 def getNewFile():
 		images = os.listdir('./images')
@@ -11,14 +12,22 @@ def getNewFile():
    		return 'images/'+ img 
 
 if __name__ ==  '__main__':
+	p1 = None
+	p2 = None
 
 	while True: 
-		fileName = getNewFile()
-		print fileName
-		p1 = subprocess.Popen(['python', 'ntscEncode.py',fileName])
-		p2 = subprocess.Popen(['python', 'ntsc_hackrf.py'])
-		time.sleep(4)
-		p2.kill()
+		try:
+			fileName = getNewFile()
+			print fileName
+			p1 = subprocess.Popen(['python', 'ntscEncode.py',fileName])
+			p2 = subprocess.Popen(['python', 'ntsc_hackrf.py'])
+			time.sleep(4)
+			p2.kill()
 
-		print "HEREEEEE"
+			print "HEREEEEE"
+		except KeyboardInterrupt:
+			print "Killing hackrf. Bye!"
+			p2.kill()
+			sys.exit()
+
 		
